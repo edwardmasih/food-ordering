c20f638478ea9c613ee07167556fe40faa7219b9
Edward Masih <masih.edward45@gmail.com>
2022-04-30 23:40:50 +0530

    rendering random images in home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { FoodService } from './../services/food/food.service';
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -8,19 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   constructor(private foodService: FoodService) {}
-  
-  public foodItemList: string[] = this.foodService.foodItem
+
+  public foodItemList: string[] = this.foodService.foodItem;
+  public finalMenu: any = []
 
   ngOnInit(): void {
-
     // let foodItemCategory = this.foodItemList[this.getRandomInt(0,this.foodItemList.length)]
     // this.foodService.getFoodImage(foodItemCategory).subscribe(data => {
     //   console.log(data)
     // });
+
+    for (let i = 0; i < this.foodItemList.length; i++) {
+      this.finalMenu[i] = []
+      for (let j = 0; j < 5; j++) {
+        this.foodService.getFoodImage(this.foodItemList[i]).subscribe(data => {
+          // console.log(data)
+          this.finalMenu[i].push(data)
+        });
+      }
+    }
+    console.log(this.finalMenu);
     
   }
-
-
 
   getRandomInt(min: number, max: number) {
     min = Math.ceil(min);
